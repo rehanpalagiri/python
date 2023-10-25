361a72bcfb9935197d8321fefbf0d9a8205526ca
Rehan Palagiri <rehan@Khaders-Air.lan>
2023-10-24 17:21:47 -0700

    my first math python program

---
+++ prg_1.py
@@ -0,0 +1,23 @@
+def sum(a, b):
+  return a + b
+
+s = sum(2, 1)
+print("2+1=",s)
+
+
+def mul(a, b):
+  return a * b
+s = mul(2, 1)
+print("2 x 1: ",s)
+
+
+def sub (a,b):
+    return a-b
+s = sub (2, 1)
+print("2-1",s)
+
+
+def div (a,b):
+    return a/b
+s = div (1,2)
+print("1/2",s)
