07cb9ee96b1371b446fc63add10d46c226688194
Rehan Palagiri <ikhader@icloud.com>
2023-10-27 20:39:33 -0700

    my firts if condition

---
+++ if_cond.py
@@ -0,0 +1,16 @@
+def sum ( a , b, c , d ):
+    return a+b+c+d
+s=sum (1,2,3,4)
+print ("1 +2+3+4=",s)
+
+def mul (a,b,c,d):
+    return a*b*c*d
+m=mul (1,2,3,4)
+print("1*2*3*4",m)
+
+def iseven (a):
+    if a%2==0:
+        return True
+    return False 
+s= iseven (1)
+print("is 1 odd?",s)
