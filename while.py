0361c764e44718f4841931d70b3031bf376ab73e
Rehan Palagiri <ikhader@icloud.com>
2024-01-06 17:27:25 -0800

    add while loop

---
+++ while.py
@@ -0,0 +1,29 @@
+def ps(N):
+    index=1
+    while (index <= N):
+        print ("hello: ", index)
+        index = index +1 
+ps(5)  
+
+def sumOfNumbers(n):
+    index=1
+    sum=0
+    while (index<=n):
+        sum = sum + index 
+        index = index + 1
+    return sum 
+
+s = sumOfNumbers(5)
+print ("first 5 natural numbers",s)
+
+s = sumOfNumbers(3)
+print ("first 3 natural numbers",s)
+
+
+def printEvenNumbers(n):
+    index=1
+    while (index <= n):
+        if index%2==0:
+            print (index)
+        index= index +1 
+printEvenNumbers(5)
